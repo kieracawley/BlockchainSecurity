b2a8a45bef64085a8f1f6f9cc125bc77799d73da
Kiera Cawley <kieracawley@gmail.com>
2021-01-18 23:43:56 -0500

    readme

--- KCoin/main.js
+++ KCoin/main.js
@@ -3,10 +3,10 @@ const {Blockchain, Transaction} = require('./blockchain');
 const EC = require('elliptic').ec;
 const ec = new EC('secp256k1');
 
-const myKey = ec.keyFromPrivate("b3d187d5e7ffca95f986218f4340f20f30fd09101eab2052f93a6233e47d4cb1");
+const myKey = ec.keyFromPrivate("PRIVATE_KEY_HERE");
 const myWalletAddress = myKey.getPublic("hex");
 
-const friend = "04d1730f704e6ed77bfd65ea83732c8c7492e22a2482350809b05cff1a22d50dea7c2ecc970c95b6024e0c61510aec9ae95d5741e7cd3764020599a0ec1a834b48";
+const friend = "PUBLIC_KEY_HERE";
 
 let kcoin = new Blockchain();
 
---
+++ Readme.md
@@ -0,0 +1,16 @@
+REQUIREMENTS
+------------
+
+nodejs (https://nodejs.org/en/)
+
+HOW TO USE
+------------
+1) Clone repo and navigate to KCoin directory
+
+2) `npm install --save crypto-js`
+
+3) `npm install elliptic`
+
+4) Run `node keyGenerator.js` twice to generate two key pairs
+
+5) Fill in the private key of one user and the public key of the other in main.js
